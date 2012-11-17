3bfc9b5f3fdbdbfeb2347c8ea467995687340d46
Quentin SANTOS <quentin.santos@worldonline.fr>
2012-11-17 21:12:37 +0100

    Removed types.h, moved proxy connection to function

--- main.c
+++ main.c
@@ -6,22 +6,50 @@
 #include <sys/select.h>
 #include <unistd.h>
 
-#include "types.h"
 #include "socket.h"
 
-void passThrough(u32 from, u32 to)
+void passThrough(int from, int to)
 {
 	char buffer[256];
-	u32 n = read(from, buffer, 256);
+	int n = read(from, buffer, 256);
 	write(to, buffer, n);
+}
+
+int proxyto(int socket, char* host, char* port)
+{
+	static char greeting[9] = { 0x04, 0x01 };
+	static char buffer[8];
+	
+	char isIP = inet_pton(AF_INET, host, greeting + 4);
+	if (!isIP)
+	{
+		greeting[4] = 0;
+		greeting[5] = 0;
+		greeting[6] = 0;
+		greeting[7] = 1;
+		printf("%s\n", host);
+	}
+	short ns_port = htons(atoi(port));
+	memcpy(greeting+2, &ns_port, 2);
+	
+	write(socket, greeting, 9);
+	if (!isIP)
+		write(socket, host, strlen(host)+1);
+	read(socket, buffer, 8);
+	char status = buffer[1];
+	
+	if (status == 0x5A)
+		return 0;
+	else
+		return -1;
 }
 
 int main(int argc, char** argv)
 {
 	assert(argc > 1);
 	
-	string host = strtok(argv[1], ":");
-	string port = strtok(NULL, "");
+	char* host = strtok(argv[1], ":");
+	char* port = strtok(NULL, "");
 	int proxy = TCP_Connect(host, port);
 	if (proxy < 0)
 	{
@@ -29,48 +57,16 @@ int main(int argc, char** argv)
 		return 1;
 	}
 	
-	for (s32 i = 2; i < argc; i++)
+	for (int i = 2; i < argc; i++)
 	{
-		static u8 greeting[9] = { 0x04, 0x01 };
-		static u8 buffer[8];
-		
 		host = strtok(argv[i], ":");
 		port = strtok(NULL, "");
-		bool isIP = inet_pton(AF_INET, host, greeting + 4);
-		if (!isIP)
+		if (proxyto(proxy, host, port) < 0)
 		{
-			greeting[4] = 0;
-			greeting[5] = 0;
-			greeting[6] = 0;
-			greeting[7] = 1;
-			printf("%s\n", host);
+			fprintf(stderr, "no %i: Could not reach final node (%s)\n", i-1, host);
+			close(proxy);
+			return -1;
 		}
-		*(u16*)(greeting+2) = htons(atoi(port));
-		
-		write(proxy, greeting, 9);
-		if (!isIP)
-			write(proxy, host, strlen(host)+1);
-		read(proxy, buffer, 8);
-		u8 status = buffer[1];
-		
-		if (status == 0x5A)
-			continue;
-		
-		if (status == 0x5B)
-		{
-			if (i == argc - 1)
-				fprintf(stderr, "no %lu: Could not reach final target (%s)\n", i-1, host);
-			else
-				fprintf(stderr, "no %lu: Could not reach proxy no %lu (%s)\n", i-1, i, host);
-		}
-		else if (status == 0x5C)
-			fprintf(stderr, "no %lu: Not running identd (client not reachable)\n", i-1);
-		else if (status == 0x5D)
-			fprintf(stderr, "no %lu: Authentication failed\n", i-1);
-		else
-			fprintf(stderr, "no %lu: Unknown status from server (%2.X)\n", i-1, status);
-		close(proxy);
-		return 1;
 	}
 	
 	fd_set fds;
